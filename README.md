80f440a95f54f36703e23864bb9d49f0b7ad6e96
Hanen-bensaid <>
2023-11-19 13:24:32 +0100

    add project student note
    js code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# studentnote
+# StudentsNOte
+Students
---
+++ js/min.js
@@ -0,0 +1,15 @@
+//Recuperer les valeurs des champs du formulaire
+const fullName= document.querySelector('.fullName');
+const dateOfBirth= document.querySelector('dateOfBirth');
+const gender= document.querySelector('select[name="gender"]');
+const task1= document.querySelector('.task1');
+const task2= document.querySelector('.task2');
+const task3= document.querySelector('.task3');
+// recuperer les boutons
+const result= document.querySelector('.Result');
+const submit= document.querySelector('.Submit');
+const reset= document.querySelector('.Reset');
+// validation rules fullName
+function validateFullName(fullName){
+    
+}
